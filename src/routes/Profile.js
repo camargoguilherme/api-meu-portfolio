62c6a6575bf7c1030dd014450d569296128d3bf7
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-06-20 20:03:09 -0300

    Teminada configuracao para upload de arquivos

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -12,7 +12,7 @@ const uploadCurriculum = multer(uploadsConfig);
 routes.get('/profile', ProfileController.findAll);
 routes.get('/profile/:id', ProfileController.show);
 routes.post('/profile', uploadAvatar.fields([{ name: 'avatar', maxCount: 1} , {name: 'curriculum', maxCount: 1}]),ProfileController.store);
-routes.put('/profile/:id', ProfileController.update);
+routes.put('/profile/:id', uploadAvatar.fields([{ name: 'avatar', maxCount: 1} , {name: 'curriculum', maxCount: 1}]), ProfileController.update);
 routes.delete('/profile/:id', ProfileController.delete);
 routes.delete('/profile', ProfileController.deleteMultiple);
 
